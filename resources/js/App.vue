54ce7a07b3048206eccc8e959c556919d8c5b2db
João Marcos <joaomarcos.src@gmail.com>
2023-02-10 11:08:25 -0300

    :wrench: build: install and config App.vue/vuetify

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app :dark="isDark">
+        <v-main>
+            <!-- <router-view :key="$route.fullPath"/> -->
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+        }
+    },
+    computed: {
+        isDark() {
+            return false;
+        },
+    },
+}
+</script>
+
+<style >
+@import url('https://fonts.googleapis.com/css?family=Ropa+Sans');
+* {
+    font-family: 'Ropa Sans', Courier, monospace;
+}
+</style>
